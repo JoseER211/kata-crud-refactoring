4d6c00990dea61504c1977169cfcc5d874e7ecbf
JoseER <echavarria11josedavid@gmail.com>
2022-02-24 16:20:57 -0500

    Se agregaron los archivos: CatalogueController, CatalogueService, y se modificaron los archivos: TodoController, TodoService

--- back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
+++ back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
@@ -1,9 +1,15 @@
 package co.com.sofka.crud.controllers;
 
+import co.com.sofka.crud.dtos.TodoDTO;
 import co.com.sofka.crud.services.TodoService;
 import co.com.sofka.crud.models.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //Hace el llamado del dato y lo pasa a la interfaz
 @RestController
@@ -14,31 +20,45 @@ public class TodoController {
     private TodoService service;
 
     @GetMapping(value = "api/todos")
-    public Iterable<Todo> list(){
+    public Iterable<TodoDTO> list() {
         return service.list();
     }
-    
+
     @PostMapping(value = "api/todo")
-    public Todo save(@RequestBody Todo todo){
-        return service.save(todo);
+    public Todo save(@RequestBody TodoDTO todoDTO) {
+        return service.save(todoDTO);
     }
 
     @PutMapping(value = "api/todo")
-    public Todo update(@RequestBody Todo todo){
-        if(todo.getId() != null){
-            return service.save(todo);
+    public Todo update(@RequestBody TodoDTO todoDTO) {
+        if (todoDTO.getId() != null) {
+            return service.save(todoDTO);
         }
         throw new RuntimeException("No existe el id para actualizar");
     }
 
     @DeleteMapping(value = "api/{id}/todo")
-    public void delete(@PathVariable("id")Long id){
+    public void delete(@PathVariable("id") Long id) {
         service.delete(id);
     }
 
     @GetMapping(value = "api/{id}/todo")
-    public Todo get(@PathVariable("id") Long id){
-        return service.get(id);
+    public TodoDTO get(@PathVariable("id") Long id) {
+        return service.getDTO(id);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldError = "error";
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldError, errorMessage);
+        });
+
+        return errors;
+    }
 }
--- back/src/main/java/co/com/sofka/crud/services/TodoService.java
+++ back/src/main/java/co/com/sofka/crud/services/TodoService.java
@@ -1,9 +1,15 @@
 package co.com.sofka.crud.services;
 
+import co.com.sofka.crud.Connector;
+import co.com.sofka.crud.dtos.TodoDTO;
 import co.com.sofka.crud.models.Todo;
+import co.com.sofka.crud.repositories.CatalogueRepository;
 import co.com.sofka.crud.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //Gestiona el acceso al dato
 
@@ -13,20 +19,35 @@ public class TodoService {
     @Autowired
     private TodoRepository repository;
 
-    public Iterable<Todo> list(){
-        return repository.findAll();
+    @Autowired
+    private CatalogueRepository catalogueRepository;
+
+    public Iterable<TodoDTO> list(){
+
+        Iterable<Todo> todos = repository.findAll();
+        List<TodoDTO> todoDTOS = new ArrayList<>();
+        for(Todo todo: todos){
+            todoDTOS.add(Connector.makeTodoDTO(todo));
+        }
+        return todoDTOS;
     }
 
-    public Todo save(Todo todo){
-        return repository.save(todo);
-    }
+    public Todo save(TodoDTO todoDTO){
+        Todo todo = Connector.makeTodo(todoDTO);
+        todo.setCatalogue(catalogueRepository.findById(todoDTO.getCatalogueId()).orElseThrow());
+        return repository.save(todo);}
 
     public void delete(Long id){
         repository.delete(get(id));
     }
 
     public Todo get(Long id){
-         return repository.findById(id).orElseThrow();
+        return repository.findById(id).orElseThrow();
+    }
+
+    public TodoDTO getDTO(Long id){
+        Todo todo = get(id);
+        return Connector.makeTodoDTO(todo);
     }
 
 }
