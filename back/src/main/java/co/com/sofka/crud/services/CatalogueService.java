4ec15dd6d55054f04d30aef6fc81b8505bf2c1a5
JoseER <echavarria11josedavid@gmail.com>
2022-02-24 16:32:53 -0500

    Se agregó la anotación @Service a CatalogueService

--- back/src/main/java/co/com/sofka/crud/services/CatalogueService.java
+++ back/src/main/java/co/com/sofka/crud/services/CatalogueService.java
@@ -10,10 +10,12 @@ import co.com.sofka.crud.models.Todo;
 import co.com.sofka.crud.repositories.CatalogueRepository;
 import co.com.sofka.crud.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CatalogueService {
 
     @Autowired
